d18c41340b125e5604176a822ad63da60f1b9db1
jrocia <69405400+jrocia@users.noreply.github.com>
2024-03-06 07:33:35 -0300

    Create test.ps1

---
+++ scripts/test.ps1
@@ -0,0 +1,11 @@
+echo "${env:WORKSPACE}    ${env:BUILD_NUMBER}"
+
+echo "${env.WORKSPACE}    ${env.BUILD_NUMBER}"
+
+echo "${WORKSPACE}    ${BUILD_NUMBER}"
+
+echo "$env:WORKSPACE    $env:BUILD_NUMBER"
+
+echo "$env.WORKSPACE    $env.BUILD_NUMBER"
+
+echo "$WORKSPACE    $BUILD_NUMBER"
