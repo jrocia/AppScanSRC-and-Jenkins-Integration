b03e9d297f30c8908f96fbf3683e03d2ecd84181
jrocia <69405400+jrocia@users.noreply.github.com>
2024-03-06 07:37:27 -0300

    Update test.ps1

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,11 +1,17 @@
+echo '${env:WORKSPACE}    ${env:BUILD_NUMBER}'
 echo "${env:WORKSPACE}    ${env:BUILD_NUMBER}"
 
+echo '${env.WORKSPACE}    ${env.BUILD_NUMBER}'
 echo "${env.WORKSPACE}    ${env.BUILD_NUMBER}"
 
+echo '${WORKSPACE}    ${BUILD_NUMBER}'
 echo "${WORKSPACE}    ${BUILD_NUMBER}"
 
+echo '$env:WORKSPACE    $env:BUILD_NUMBER'
 echo "$env:WORKSPACE    $env:BUILD_NUMBER"
 
+echo '$env.WORKSPACE    $env.BUILD_NUMBER'
 echo "$env.WORKSPACE    $env.BUILD_NUMBER"
 
+echo '$WORKSPACE    $BUILD_NUMBER'
 echo "$WORKSPACE    $BUILD_NUMBER"
